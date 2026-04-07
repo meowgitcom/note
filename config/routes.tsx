660f9693a2d9b047985937c2ad309fd3d6fdabe0
sam <aquaticcalf@proton.me>
2026-04-08 01:34:13 +0530

    open[note]

---
+++ config/routes.tsx
@@ -0,0 +1,24 @@
+import { StyleSheet, View } from "react-native"
+import { Text } from "react-native-paper"
+
+export default function Routes() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>open[note]</Text>
+    </View>
+  )
+}
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    fontSize: 32,
+    fontWeight: "ultralight",
+    marginBottom: 20,
+    fontFamily: "GeistMono_400Regular",
+  },
+})
