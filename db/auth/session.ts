f98e9c42dbefd1300b32ee3ce7a252522057cef9
sam <aquaticcalf@proton.me>
2026-04-10 21:01:33 +0530

    auth

---
+++ db/auth/session.ts
@@ -0,0 +1,15 @@
+import { text, integer, sqliteTable } from "drizzle-orm/sqlite-core"
+import { user } from "./user"
+
+export const session = sqliteTable("session", {
+  id: text("id").primaryKey(),
+  expiresAt: integer("expires_at", { mode: "timestamp_ms" }).notNull(),
+  token: text("token").notNull().unique(),
+  createdAt: integer("created_at", { mode: "timestamp_ms" }).notNull(),
+  updatedAt: integer("updated_at", { mode: "timestamp_ms" }).notNull(),
+  ipAddress: text("ip_address"),
+  userAgent: text("user_agent"),
+  userId: text("user_id")
+    .notNull()
+    .references(() => user.id, { onDelete: "cascade" }),
+})
---
+++ db/auth/verification.ts
@@ -0,0 +1,10 @@
+import { text, integer, sqliteTable } from "drizzle-orm/sqlite-core"
+
+export const verification = sqliteTable("verification", {
+  id: text("id").primaryKey(),
+  identifier: text("identifier").notNull(),
+  value: text("value").notNull(),
+  expiresAt: integer("expires_at", { mode: "timestamp_ms" }).notNull(),
+  createdAt: integer("created_at", { mode: "timestamp_ms" }),
+  updatedAt: integer("updated_at", { mode: "timestamp_ms" }),
+})
