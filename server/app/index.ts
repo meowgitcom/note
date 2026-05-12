fd481c08eb2a5e50895c951032bbd2487992f5b2
sam <aquaticcalf@proton.me>
2026-05-13 01:41:48 +0530

    ox and deploy

--- server/app/index.ts
+++ server/app/index.ts
@@ -1,15 +1,17 @@
-import { Hono } from "hono"
+// @ts-ignore : kotlin bridge
 import { getWelcomeMessage, calculateScore } from "../dist/server.mjs"
+
+import { Hono } from "hono"
 
 const app = new Hono()
 
-app.get('/', (c) => {
-  const name = c.req.query('name') || 'Guest'
+app.get("/", (c) => {
+  const name = c.req.query("name") || "Guest"
   return c.text(getWelcomeMessage(name))
 })
 
-app.get('/score/:val', (c) => {
-  const val = parseInt(c.req.param('val'))
+app.get("/score/:val", (c) => {
+  const val = parseInt(c.req.param("val"))
   const score = calculateScore(val)
   return c.json({ score })
 })
