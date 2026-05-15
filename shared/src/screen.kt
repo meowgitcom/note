cb9e3b22355c5deb3443e9207ebb5f2b2e1d7c62
sam <aquaticcalf@proton.me>
2026-05-16 01:27:04 +0530

    font test

--- shared/src/screen.kt
+++ shared/src/screen.kt
@@ -7,9 +7,12 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.compose.foundation.isSystemInDarkTheme
 import shared.config.baseurl
 import shared.style.Colors
+import shared.style.H2
+import shared.style.InlineCode
 
 @Composable
 fun Screen() {
@@ -17,11 +20,11 @@ fun Screen() {
     MaterialTheme {
         Column(
             modifier = Modifier.fillMaxSize().background(palette.background),
-            verticalArrangement = Arrangement.Center,
+            verticalArrangement = Arrangement.spacedBy(16.dp, Alignment.CenterVertically),
             horizontalAlignment = Alignment.CenterHorizontally,
         ) {
-            Text("Hello, ${getWorld()}!", color = palette.foreground)
-            Text("API URL : $baseurl", color = palette.foreground)
+            Text("Hello, ${getWorld()}!", style = H2.copy(color=palette.foreground))
+            Text("API URL : $baseurl", style = InlineCode.copy(color = palette.foreground))
         }
     }
 }
